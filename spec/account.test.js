e64782aca3d9910b81434841b4dbc6337720bc2c
mbrad26 <mbrad26@gmail.com>
2020-12-13 15:05:50 +0000

    Transaction is defined

--- spec/account.test.js
+++ spec/account.test.js
@@ -1,8 +1,24 @@
 const Account = require('../lib/account');
+const Transaction = require('../lib/transaction');
+
+jest.mock('../lib/transaction');
 
 describe('Account', () => {
+  let account;
+
+  beforeEach(() => {
+    account = new Account();
+  });
+
   it('is defined', () => {
-    const account = new Account();
     expect(account).toBeDefined();
+  });
+
+  it('can make a deposit', () => {
+    const addTransaction = jest.fn();
+    account.deposit(50);
+
+    expect(addTransaction).toHaveBeenCalledTimes(1);
+    expect(addTransaction).toHaveBeenCalledWith(50);
   });
 });
---
+++ spec/transaction.test.js
@@ -0,0 +1,7 @@
+const Transaction = require('../lib/transaction');
+
+describe('Transaction', () => {
+  it('is defined', () => {
+    expect(Transaction).toBeDefined();
+  });
+});
