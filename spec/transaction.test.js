7b107210fe8b51bb9f28722965c6699f6d2563d5
mbrad26 <mbrad26@gmail.com>
2020-12-17 12:07:20 +0000

    addDepositTransaction registers a deposit transaction

--- spec/transaction.test.js
+++ spec/transaction.test.js
@@ -1,7 +1,23 @@
 const Transaction = require('../lib/transaction');
 
 describe('Transaction', () => {
+  beforeEach(() => {
+    Date.now = jest.fn(() => 1608204215428);
+  });
+
   it('is defined', () => {
     expect(Transaction).toBeDefined();
+  });
+
+  describe('addDepositTransaction', () => {
+    it('registers a deposit transaction', () => {
+      const transaction = new Transaction(50, null);
+
+      expect(Transaction.transactions).toEqual([]);
+
+      Transaction.addDepositTransaction(50);
+
+      expect(Transaction.transactions).toContainEqual(transaction);
+    });
   });
 });
